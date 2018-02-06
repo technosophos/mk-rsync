b17d4e03a4488951d7c0d546d1cdc01f241a957e
Matt Butcher <matt.butcher@microsoft.com>
2018-02-06 13:44:18 -0700

    Use a more reasonable set of rsync flags
    Also fixed a typo in the brigade.js

--- brigade.js
+++ brigade.js
@@ -2,7 +2,7 @@ const { events, Job } = require("brigadier");
 
 // Quick test to see if the files are actually mounted
 events.on("exec", () => {
-  (new Job("a", "alpine:3.6", ["ls -lah /src"])).run(res => {
+  (new Job("a", "alpine:3.6", ["ls -lah /src/mk-rsync"])).run().then(res => {
     console.log(res.toString())
   })
 });
--- rsync-sidecar/sync.sh
+++ rsync-sidecar/sync.sh
@@ -7,6 +7,10 @@
 
 echo "Copying ${VCS_REPO} to ${VCS_LOCAL_PATH} (HOST: ${RSYNC_RSYNC_SERVICE_HOST})"
 
-rsync -Cat rsync://${RSYNC_RSYNC_SERVICE_HOST}/share/${VCS_REPO} ${VCS_LOCAL_PATH}
+# -a = -rlptgoD
+# Don't want o or g b/c users aren't moved
+# Don't want D because we can't transfer devices
+# Might not want p because users are not preserved
+rsync -Ctrlp rsync://${RSYNC_RSYNC_SERVICE_HOST}/share/${VCS_REPO} ${VCS_LOCAL_PATH}
 
 echo "Done copying"
